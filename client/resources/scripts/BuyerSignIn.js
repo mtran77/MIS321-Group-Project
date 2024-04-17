f94a77690c8e2b0e57ea8363042a53e56c479fa5
Carson Burian <carson@Carsons-MacBook-Pro.local>
2024-04-17 09:33:25 -0500

    Changes SignIn

---
+++ client/resources/scripts/BuyerSignIn.js
@@ -0,0 +1,20 @@
+function GetZipcode (){
+    // gives the code the zipcode that will sort and only display listings with 
+    // matching zipcode to the buyer
+    var zipcode = document.getElementById("buyerZipcode").value;
+
+    
+    fetch(`/api/listings/${zipcode}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('No Network Response'); //Error handling in console
+            }
+            return response.json();
+        })
+        .then(listingsData => {
+            console.log(listingsData); 
+        })
+        .catch(error => {
+            console.error('Fetch operation error:', error); //More error handling in console
+        });
+}
