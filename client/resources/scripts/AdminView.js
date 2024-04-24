90b22f45cd1b7827dc24c857c181efc1453d217a
My Tran <mtran7@crimson.ua.edu>
2024-04-24 18:17:49 -0500

    .

--- client/resources/scripts/AdminView.js
+++ client/resources/scripts/AdminView.js
@@ -3,22 +3,16 @@ function DisplayDescription() {
     // getElementByID(descriptionModel 000000) 
     //should get the description id + listing id / whatever identifies listing that was passed in from the parameter
     var modal = document.getElementById("descriptionModal 000000");
-    var span = document.getElementsByClassName("close")[0];
 
-    if (modal && span) {
+    if (modal) {
         modal.style.display = "block";
 
         //closes modal
-        span.onclick = function() {
-        modal.style.display = "none";
-        }
         window.onclick = function(event) {
             if (event.target == modal) {
                 modal.style.display = "none";
             }
         }
-    } else {
-        console.error("Modal or close button element not found.");
     }
 }
 
@@ -27,21 +21,14 @@ function DisplayImage() {
     // getElementByID(descriptionModel 000000) 
     //should get the description id + listing id / whatever identifies listing that was passed in from the parameter
     var modal = document.getElementById("imageModal 000000");
-    var span = document.getElementsByClassName("close1")[0];
   
-    if (modal && span) {
+  
+    if (modal ) {
         modal.style.display = "block";
-
-        span.onclick = function() {
-        modal.style.display = "none";
-        }
-
         window.onclick = function(event) {
             if (event.target == modal) {
                 modal.style.display = "none";
             }
         }
-    } else {
-        console.error("Modal or close button element not found.");
     }
 }
